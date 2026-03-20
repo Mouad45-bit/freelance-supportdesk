f94513a4c42e59aa88910b245e940465b5ded47e
mouadfekkak <mouad@192.168.119.131>
2026-03-20 00:24:08 +0000

    create BaseEntity

---
+++ src/main/java/com/example/supportdesk/common/entity/BaseEntity.java
@@ -0,0 +1,26 @@
+package com.example.supportdesk.common.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter @Setter
+public class BaseEntity {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    //
+    @CreatedDate
+    @Column(name = "created_at",nullable = false, updatable = false)
+    private Instant createdAt;
+    //
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private Instant updatedAt;
+}

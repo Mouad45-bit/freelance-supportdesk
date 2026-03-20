c5a266407c6def7c041de3b1c9c8cc60c66878ac
mouadfekkak <mouad@192.168.119.131>
2026-03-20 10:58:00 +0000

    set class to 'abstract' and remove Setter

--- src/main/java/com/example/supportdesk/common/entity/BaseEntity.java
+++ src/main/java/com/example/supportdesk/common/entity/BaseEntity.java
@@ -2,7 +2,6 @@ package com.example.supportdesk.common.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
-import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -11,8 +10,8 @@ import java.time.Instant;
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-@Getter @Setter
-public class BaseEntity {
+@Getter
+public abstract class BaseEntity {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     //

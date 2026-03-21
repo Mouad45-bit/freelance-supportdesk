c2b168081bedc9c910a46d92b1ee5bdc5158e7f5
mouadfekkak <mouad@192.168.119.131>
2026-03-21 22:51:29 +0000

    create Ticket

---
+++ src/main/java/com/example/supportdesk/ticket/entity/Ticket.java
@@ -0,0 +1,40 @@
+package com.example.supportdesk.ticket.entity;
+
+import com.example.supportdesk.common.entity.BaseEntity;
+import com.example.supportdesk.common.enums.TicketPriority;
+import com.example.supportdesk.common.enums.TicketStatus;
+import com.example.supportdesk.user.entity.AppUser;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity @Table(name = "tickets")
+@Getter @NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Ticket extends BaseEntity {
+    @Column(name = "title", nullable = false, length = 150)
+    private String title;
+    @Column(name = "description", nullable = false, columnDefinition = "TEXT")
+    private String description;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false, length = 20)
+    private TicketStatus status;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "priority", nullable = false, length = 20)
+    private TicketPriority priority;
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "author_id",  nullable = false)
+    private AppUser author;
+    //
+    public Ticket(String title, String description, TicketStatus status, TicketPriority priority, AppUser author) {
+        this.title = title;
+        this.description = description;
+        this.status = status;
+        this.priority = priority;
+        this.author = author;
+    }
+
+    public void changeStatus(TicketStatus newStatus) {
+        this.status = newStatus;
+    }
+}

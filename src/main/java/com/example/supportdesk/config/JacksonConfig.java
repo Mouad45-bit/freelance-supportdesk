10db775fd2375e4133720225eff09bd4c90774df
mouadfekkak <mouad@192.168.119.131>
2026-03-22 20:51:01 +0100

    create JacksonConfig

---
+++ src/main/java/com/example/supportdesk/config/JacksonConfig.java
@@ -0,0 +1,13 @@
+package com.example.supportdesk.config;
+
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfig {
+    @Bean
+    public JsonMapper jsonMapper() {
+        return new JsonMapper();
+    }
+}

adf83718d59899b9ce89b501b83967ee853e492f
mouadfekkak <mouad@192.168.119.131>
2026-03-23 19:55:44 +0100

    add findByCommentGroupId

--- src/main/java/com/example/supportdesk/comment/repository/TicketCommentVersionRepository.java
+++ src/main/java/com/example/supportdesk/comment/repository/TicketCommentVersionRepository.java
@@ -1,7 +1,12 @@
 package com.example.supportdesk.comment.repository;
 
 import com.example.supportdesk.comment.entity.TicketCommentVersion;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.UUID;
+
 public interface TicketCommentVersionRepository extends JpaRepository<TicketCommentVersion, Long> {
+    Page<TicketCommentVersion> findByCommentGroupId(UUID commentGroupId, Pageable pageable);
 }

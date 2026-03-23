7f9b5d1c9447a13aaf7fc89c4a336aa3e8c52596
mouadfekkak <mouad@192.168.119.131>
2026-03-23 19:54:11 +0100

    add findByTicketId

--- src/main/java/com/example/supportdesk/comment/repository/TicketCommentRepository.java
+++ src/main/java/com/example/supportdesk/comment/repository/TicketCommentRepository.java
@@ -1,7 +1,10 @@
 package com.example.supportdesk.comment.repository;
 
 import com.example.supportdesk.comment.entity.TicketComment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TicketCommentRepository extends JpaRepository<TicketComment, Long> {
+    Page<TicketComment> findByTicketId(Long ticketId, Pageable pageable);
 }

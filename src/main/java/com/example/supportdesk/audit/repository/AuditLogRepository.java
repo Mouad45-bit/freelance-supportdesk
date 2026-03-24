f532ec76a38b6aaef2ae565469fe680eaf74854c
mouad45-bit <mouadfekkak2002@gmail.com>
2026-03-24 15:18:40 +0100

    create AuditLogRepository

---
+++ src/main/java/com/example/supportdesk/audit/repository/AuditLogRepository.java
@@ -0,0 +1,9 @@
+package com.example.supportdesk.audit.repository;
+
+import com.example.supportdesk.audit.entity.AuditLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface AuditLogRepository
+        extends JpaRepository<AuditLog, Long>, JpaSpecificationExecutor<AuditLog> {
+}

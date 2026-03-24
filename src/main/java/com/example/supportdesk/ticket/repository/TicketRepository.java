b018448910367f1e0def12cb2522e1fe05d575db
mouad45-bit <mouadfekkak2002@gmail.com>
2026-03-24 22:54:05 +0100

    add findByIdAndDeletedFalse

--- src/main/java/com/example/supportdesk/ticket/repository/TicketRepository.java
+++ src/main/java/com/example/supportdesk/ticket/repository/TicketRepository.java
@@ -4,5 +4,8 @@ import com.example.supportdesk.ticket.entity.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.Optional;
+
 public interface TicketRepository extends JpaRepository<Ticket,Long>, JpaSpecificationExecutor<Ticket> {
+    Optional<Ticket> findByIdAndDeletedFalse(Long id);
 }

43cef77ae1f80807c3073c3b8a16d7070a472453
mouad45-bit <mouadfekkak2002@gmail.com>
2026-03-25 00:07:26 +0100

    add findBy*AndDeletedFalse

--- src/main/java/com/example/supportdesk/comment/repository/TicketCommentRepository.java
+++ src/main/java/com/example/supportdesk/comment/repository/TicketCommentRepository.java
@@ -5,6 +5,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TicketCommentRepository extends JpaRepository<TicketComment, Long> {
-    Page<TicketComment> findByTicketId(Long ticketId, Pageable pageable);
+    Page<TicketComment> findByTicketIdAndDeletedFalse(Long ticketId, Pageable pageable);
+    Optional<TicketComment> findByIdAndDeletedFalse(Long id);
 }

fa36931a94fa799c7608988643a4cc91c4a56e48
mouad45-bit <mouadfekkak2002@gmail.com>
2026-03-25 21:12:03 +0100

    change details to Object

--- src/main/java/com/example/supportdesk/audit/dto/AuditLogResponse.java
+++ src/main/java/com/example/supportdesk/audit/dto/AuditLogResponse.java
@@ -1,6 +1,5 @@
 package com.example.supportdesk.audit.dto;
 
-import com.example.supportdesk.audit.entity.AuditLog;
 import com.example.supportdesk.common.enums.AuditAction;
 import com.example.supportdesk.common.enums.AuditResourceType;
 
@@ -14,18 +13,6 @@ public record AuditLogResponse(
         Long actorId,
         String actorUsername,
         Instant timestamp,
-        String details
+        Object details
 ) {
-    public static AuditLogResponse from(AuditLog log){
-        return new AuditLogResponse(
-                log.getId(),
-                log.getAction(),
-                log.getResourceType(),
-                log.getResourceId(),
-                log.getActor().getId(),
-                log.getActor().getUsername(),
-                log.getTimestamp(),
-                log.getDetails()
-        );
-    }
 }

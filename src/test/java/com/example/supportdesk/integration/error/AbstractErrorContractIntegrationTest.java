16d36d60327ca7829edbd01730839ce6ef52afb1
mouad45-bit <mouadfekkak2002@gmail.com>
2026-04-02 21:31:43 +0100

    create AbstractErrorContractIntegrationTest

---
+++ src/test/java/com/example/supportdesk/integration/error/AbstractErrorContractIntegrationTest.java
@@ -0,0 +1,6 @@
+package com.example.supportdesk.integration.error;
+
+import com.example.supportdesk.integration.config.AbstractAuthenticatedIntegrationTest;
+
+public abstract class AbstractErrorContractIntegrationTest extends AbstractAuthenticatedIntegrationTest {
+}

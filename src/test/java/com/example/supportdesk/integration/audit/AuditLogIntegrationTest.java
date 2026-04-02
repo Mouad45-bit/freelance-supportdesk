824c82435c3a45e62f01d153e98b7079844f5f3a
mouad45-bit <mouadfekkak2002@gmail.com>
2026-04-02 21:06:49 +0100

    delete AuditLogIntegrationTest

--- src/test/java/com/example/supportdesk/integration/audit/AuditLogIntegrationTest.java
+++
@@ -1,7 +0,0 @@
-package com.example.supportdesk.integration.audit;
-
-import com.example.supportdesk.integration.config.AbstractAuthenticatedIntegrationTest;
-
-public class AuditLogIntegrationTest extends AbstractAuthenticatedIntegrationTest {
-    ////
-}

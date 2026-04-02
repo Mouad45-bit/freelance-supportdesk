00412d7fc5111d70ce3ff6c51a3d1f80642b355d
mouad45-bit <mouadfekkak2002@gmail.com>
2026-04-02 21:33:46 +0100

    delete error tests

--- src/test/java/com/example/supportdesk/integration/error/AbstractErrorContractIntegrationTest.java
+++
@@ -1,6 +0,0 @@
-package com.example.supportdesk.integration.error;
-
-import com.example.supportdesk.integration.config.AbstractAuthenticatedIntegrationTest;
-
-public abstract class AbstractErrorContractIntegrationTest extends AbstractAuthenticatedIntegrationTest {
-}

c4a9ea8ab0b8d9869853539ce94d53b2e775eb74
mouad45-bit <mouadfekkak2002@gmail.com>
2026-04-02 20:18:02 +0100

    delete CommentIntegrationTest

--- src/test/java/com/example/supportdesk/integration/comment/CommentIntegrationTest.java
+++
@@ -1,7 +0,0 @@
-package com.example.supportdesk.integration.comment;
-
-import com.example.supportdesk.integration.config.AbstractAuthenticatedIntegrationTest;
-
-public class CommentIntegrationTest extends AbstractAuthenticatedIntegrationTest {
-    ////
-}
